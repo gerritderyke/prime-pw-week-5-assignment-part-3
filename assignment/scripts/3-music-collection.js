03e9eec16f534a57057cabb83a240b281109acb2
Gerrit DeRyke <gderyke@gmail.com>
2021-06-05 13:26:41 -0500

    addtocollection functioning correctly

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,4 +2,30 @@ console.log('***** Music Collection *****')
 
  const collection = [];
 
- 
+ //-----------------------------------
+
+ // function addtoCollection( title, artist, yearPublished ){
+ //   for ( let name of addtoCollection){
+ //   const music = {
+ //     title: 'Rumors',
+ //     artist: 'Fleetwood Mac',
+ //     yearPublished: '1977',
+ //   };//end object
+ //   return music;
+ //  }//end for
+ // } //end function
+
+//------------------------------------
+
+function addtoCollection( title, artist, yearPublished ){
+
+    let music = {
+      title: title,
+      artist: artist,
+      yearPublished: yearPublished
+    }//end object
+collection.push(music)
+  return music
+}//end function
+console.log(addtoCollection('Rumors', 'Fleetwood Mac', '1977'));
+console.log(collection);
